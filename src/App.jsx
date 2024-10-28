b169f621ca75e57f316d1c1130dd7717bce40d5e
PAVAN28GIT <pavankmjp@gmail.com>
2024-10-28 20:48:40 +0530

    email sending implemented - emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Resume from './pages/Resume'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <div className="w-full bg-cover h-screen bg-right" style={{ backgroundImage: `url('/images/col.jpg')` }}>
+        <Toaster />
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import contacImg from '../../public/icons/contact-img.svg'
 
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { showToast } from "../utils/toast";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_3eyu6pf', 'template_37cju2v', form.current, {
+        publicKey: 'nGJOUXXcdWLABqoME',
+      })
+      .then(
+        () => {
+          showToast("Your message has been sent. We'll respond ASAP", 'success');
+         
+        },
+        (error) => {
+          showToast(`Failed to send message${error}`, 'error');
+        },
+      );
+  };
+
   return (
+  
     <div className='bg-gradient-to-r from-pink-600 to-purple-500 h-[82vh] pt-16' id='contact'>
     <div className='w-full px-10 py-5 flex flex-row justify-between items-center gap-2'>
 
@@ -13,14 +38,13 @@ const Contact = () => {
       <div className='mr-16'>
         <h1 className='text-5xl font-extrabold font-nunito text-nowrap pt-5 px-3'>Send a Message!! </h1>
 
-       <form className="flex flex-col gap-5 rounded-md p-5" action="">
-        <input className='px-4 w-96 py-3 bg-pink-100 outline-none rounded-lg text-black' type="text" placeholder='Enter Your Name'/>
-        <input className='px-4 w-96 py-3 bg-pink-100 outline-none rounded-lg text-black' type="email" placeholder='Your Email Address' />
-        <input className='px-4 w-96 py-3 bg-pink-100 outline-none rounded-lg text-black' type="email" placeholder='Subject' />
-     
-        <textarea className='px-4 w-96 h-40 resize-none py-3 bg-pink-100 outline-none rounded-lg text-black' type="text" placeholder='Your Message'/>
+       <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-5 rounded-md p-5" action="">
+        <input className='px-4 w-96 py-3 bg-pink-100 outline-none rounded-lg text-black' type="text" name='from_name' placeholder='Enter Your Name'/>
+        <input className='px-4 w-96 py-3 bg-pink-100 outline-none rounded-lg text-black' type="email" placeholder='Your Email Address' name='from_email' />
+    
+        <textarea className='px-4 w-96 h-40 resize-none py-3 bg-pink-100 outline-none rounded-lg text-black' type="message" placeholder='Your Message' name='message'/>
 
-        <input className='px-4 w-96 py-3 outline-none rounded-lg text-gray-200 text-2xl bg-zinc-900 hover:text-white hover:bg-black' type='submit'   />
+        <input className='px-4 w-96 py-3 outline-none rounded-lg text-gray-200 text-2xl bg-zinc-900 hover:text-white hover:bg-black' type="submit" value="Send" />
        </form>
        
    
